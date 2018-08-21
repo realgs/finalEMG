78f884f664c35c5405773d0fa86bbf26bad41bb4
Mateusz Magda <realgs@gmail.com>
2018-08-21 23:52:17 +0200

    Evaluator & optimization script for algorithm4 Hodges & Bui created

---
+++ evaluator4MOOGAScript.m
@@ -0,0 +1,8 @@
+%set the lower and upper bound
+lb = [200 0.05];
+ub = [200 0.2];
+
+%several parameters for GA optimization
+options = gaoptimset('PopulationSize',60,'PlotFcns',@gaplotpareto, 'MutationFcn', {@mutationadaptfeasible, 0.05},'TimeLimit',60);
+
+[x,fval,exitflag,output,population,scores] = gamultiobj(@evaluator4,2,[],[],[],[],lb,ub,options)
