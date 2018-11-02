cbcd9d6be51f701df3eb6b17842d549ccd9bdf64
realgs <realgs@gmail.com>
2018-11-02 17:57:58 +0100

    Deploy derivatives pre-calculated table in estimator2

--- estimators/estimator2.m
+++ estimators/estimator2.m
@@ -14,19 +14,18 @@ variabilities = zeros(5000,6);
 derivatives = diff(emg(:, 1:6));
 signsTable = sign(emg);
 smallWindow = round(windowSize / 2);
-largeWindow = windowSize * 1.5;
 
 %Counting sign changes combined with higher values of derivatives
 for c = 1:6
-    if max(abs(diff(emg(1:smallWindow,c)))) < 0.035 && max(abs(emg(:,c))) < 0.31
+    if max(abs(derivatives(1:smallWindow,c))) < 0.035 && max(abs(emg(:,c))) < 0.31
         d(c) = 0.0005;
         k(c) = 1.3;
     end
-    if max(abs(diff(emg(1:smallWindow,c)))) < 0.015 && max(abs(emg(:,c))) < 0.1
+    if max(abs(derivatives(1:smallWindow,c))) < 0.015 && max(abs(emg(:,c))) < 0.1
         d(c) = 0.00001;
         k(c) = 1.5;
     end
-    h(c) = (max(abs(diff(emg(1:largeWindow,c)))) + d(c)) * k(c);
+    h(c) = (max(abs(derivatives(1:smallWindow,c))) + d(c)) * k(c);
     %h(c) = (mean(abs(diff(emg(1:largeWindow,c))))+0.005) * k(c) * (max(emg(:,c))+0.75);
     %h(c) = k(c);
     
@@ -35,7 +34,7 @@ for c = 1:6
         %            actVar = var(emg(i-smallWindow+1:i+smallWindow-1,c));
         %            noiseVar = var(emg(1:windowSize-2,c));
         for j=(i-round(windowSize/2)+1):(i+round(windowSize/2)-1)
-            if(signsTable(j,c) == signsTable(j+1,c) && (abs(emg(j,c))-abs(emg(j+1,c))) > h(c))
+            if(signsTable(j,c) == signsTable(j+1,c) && derivatives(j,c) > h(c))
                 counter = counter+1;
             end
         end
@@ -47,7 +46,7 @@ end
 for c=1:6
     emg(c,9) = 0;
     for i = 1 : size(variabilities)
-        %Detecting condition
+        %Detection condition
         if(variabilities(i,c)>2 && abs(emg(i,c))>0.01)
             emg(c,9) = i;
             result(c) = emg(c,9) - emg(c,8);
